0dc522f135472021203d29e01a77496b0574846c
Isaias Ramirez <soote1.ir@gmail.com>
2022-11-30 22:32:02 -0600

    Implement mechanism to process map task
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -3,9 +3,11 @@ package mr
 import (
 	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/rpc"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -16,6 +18,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -30,13 +40,35 @@ func ihash(key string) int {
 //
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	GetMapTask()
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	task := GetMapTask()
+	intermediateKeyValues := ProcessMapTask(task, mapf)
+	log.Printf("intermediate key-values %v", intermediateKeyValues)
 }
 
-func GetMapTask() {
+func ProcessMapTask(t Task, mapf func(string, string) []KeyValue) []KeyValue {
+	// open input file
+	file, err := os.Open(t.InputFile)
+	if err != nil {
+		log.Fatalf("cannot open %v", t.InputFile)
+	}
+	defer file.Close()
+
+	// load split content
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", t.InputFile)
+	}
+
+	// run map function
+	intermediateKeyValues := mapf(t.InputFile, string(content))
+
+	// return sorted list of intermediate key-values
+	sort.Sort(ByKey(intermediateKeyValues))
+	return intermediateKeyValues
+}
+
+func GetMapTask() Task {
 	args := GetTaskArgs{}
 	reply := GetTaskReply{}
 
@@ -44,6 +76,8 @@ func GetMapTask() {
 	call("Coordinator.GetMapTask", &args, &reply)
 
 	log.Printf("map task: %v", reply.Task)
+
+	return reply.Task
 }
 
 //
