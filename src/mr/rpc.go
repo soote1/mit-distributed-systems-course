660f46a9ef8294f5aa62910b0d0fb25ac919a889
Isaias Ramirez <soote1.ir@gmail.com>
2022-11-30 22:13:07 -0600

    Add type definitions to fetch map task
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,24 +6,25 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
-//
-// example to show how to declare the arguments
-// and reply for an RPC.
-//
-
-type ExampleArgs struct {
-	X int
+type Task struct {
+	Type      string
+	InputFile string
 }
 
-type ExampleReply struct {
-	Y int
+type GetTaskArgs struct {
+	Worker string
+}
+
+type GetTaskReply struct {
+	Task Task
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
